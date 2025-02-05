08c2e43f3875a31e8d8e4a254b56b7d8053ca399
keyyzo <58720632+keyyzo@users.noreply.github.com>
2025-02-05 00:11:56 +0000

    added simple movement to ship

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Player Ship Movement Variabels")]
+
+    [SerializeField] float baseMovementSpeed = 5.0f;
+
+    [SerializeField] float xClampRange = 5.0f;
+    [SerializeField] float yMinClampRange = -5.0f;
+    [SerializeField] float yMaxClampRange = 5.0f;
+    
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessShipTranslation();
     }
+
 
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        movement = value.Get<Vector2>();
     }
+
+    private void ProcessShipTranslation()
+    {
+        float xOffset = movement.x * baseMovementSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
+        float yOffset = movement.y * baseMovementSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, yMinClampRange, yMaxClampRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+
 }
